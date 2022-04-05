857c558b39da89fd0442fca278021d8334b2990d
roeib <roeibarak123@gmail.com>
2022-04-06 00:25:25 +0300

    GetNormal test add in cylinder and plan

--- unitTests/geometries/CylinderTest.java
+++ unitTests/geometries/CylinderTest.java
@@ -1,5 +1,8 @@
 package geometries;
 
+import org.junit.jupiter.api.Test;
+import primitives.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -8,7 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 class CylinderTest {
+        @Test
+        void testGetNormal()
+        {
+            Cylinder c = new Cylinder(
+                    new Ray(new Point(0, 0, 1), new Vector(0, 0, 1)),
+                    2.0,
+                    3);
+            // test for point on first disk
+            assertEquals(new Vector(0,0, 1), c.getNormal(new Point(0, 1, 1)));
 
+            assertEquals(new Vector(0,1, 0), c.getNormal(new Point(0, 1, 4)));
 
-
+        }
 }
--- unitTests/geometries/PlaneTest.java
+++ unitTests/geometries/PlaneTest.java
@@ -1,6 +1,7 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +17,13 @@ class PlaneTest {
 
     @Test
     void getNormal() {
+        Point p1=new Point(1,1,1);
+        Point p2=new Point(1,1,0);
+        Point p3=new Point(1,0,1);
+        Plane plane=new Plane (p1,p2,p3);
+        assertEquals(plane.getNormal(),new Vector(-1,0,0),"ERROR:wrong value!");
+
+        assertEquals(1,plane.getNormal().length(),"ERROR: the length isn't 1");
+
     }
 }
