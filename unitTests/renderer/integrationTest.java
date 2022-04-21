f6e12b84fdbb602a687014b606f8e45fff91f503
Garusi <mgarusi101@gmail.com>
2022-04-21 09:18:44 +0300

    I added comments

--- unitTests/renderer/integrationTest.java
+++ unitTests/renderer/integrationTest.java
@@ -14,31 +14,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Integration tests for {@link Camera} class.
+ *
+ * @author  Michael and Roi
  */
 public class integrationTest {
 
-
 	/**
-	 * Test helper function to count the intersections and compare with expected value
+	 * Test  function intersections counter and compare with expected value
 	 *
-	 * @author
-	 * @param cam      camera for the test
-	 * @param geo      3D body to test the integration of the camera with
-	 * @param expected amount of intersections
+	 * @param camera      	camera
+	 * @param geometry      3D body
+	 * @param expected 		counter intersections
 	 */
-	private void assertCountIntersections(Camera cam, Intersectable geo, int expected) {
+	private void assertCountIntersections(Camera camera, Intersectable geometry, int expected) {
 		int count = 0;
 
 		List<Point> allPoints = null;
 
-		cam.setVPSize(3, 3);
-		cam.setVPDistance(1);
+		camera.setVPSize(3, 3);
+		camera.setVPDistance(1);
 		int nX =3;
 		int nY =3;
+
 		//view plane 3X3 (WxH 3X3 & nx,ny =3 => Rx,Ry =1)
 		for (int i = 0; i < nY; ++i) {
 			for (int j = 0; j < nX; ++j) {
-				var intersections = geo.findIntersections(cam.constructRay(nX, nY, j, i));
+				var intersections = geometry.findIntersections(camera.constructRay(nX, nY, j, i));
 				if (intersections != null) {
 					if (allPoints == null) {
 						allPoints = new LinkedList<>();
